5a44d05b01836e2551c7a082ad87881595de8c07
Kevin Luu <this.kevinluu@gmail.com>
2022-03-04 10:39:40 -0500

    add coins show route

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -1,5 +1,11 @@
 class CoinsController < ApplicationController
+  # fetch and return all coins
   def index
-    render json: { message: 'You reached the GET route!' }
+    render json: { message: 'You reached the INDEX route!' }
+  end
+
+  # fetch and return a specific coin
+  def show
+    render json: { message: "You reached the SHOW route! Params: #{params[:id]}!" }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'coins/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :coins, only: :index
+  resources :coins, only: [:index, :show]
 end
