3189fd37865df7eb111b8419a19cf381c3906cf4
Kevin Luu <this.kevinluu@gmail.com>
2022-03-15 15:51:49 -0400

    add user model and validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_04_141640) do
+ActiveRecord::Schema.define(version: 2022_03_15_181046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2022_03_04_141640) do
   create_table "coins", force: :cascade do |t|
     t.string "name"
     t.decimal "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
